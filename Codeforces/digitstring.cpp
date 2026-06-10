c88dcf8657e25051bc4013185eef2c2d5d430d5c
Md. Nahid Hasan <nahid1719abd@gmail.com>
2026-06-10 09:19:30 +0600

    added

---
+++ Codeforces/digitstring.cpp
@@ -0,0 +1,55 @@
+/*
+ N   N   AAAAA  H   H  III  DDDD
+ NN  N  A     A H   H   I   D   D
+ N N N  AAAAAAA HHHHH   I   D    D
+ N  NN  A     A H   H   I   D   D
+ N   N  A     A H   H  III  DDDD
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define mone cout << "-1\n"
+#define ans cout << result << "\n"
+#define countt cout << count << "\n"
+#define vl vector<long long>
+#define vi vector<int>
+#define vs vector<string>
+#define foriton(n) for (long long i = 0; i < n; i++)
+#define scan(a, n) for (long long i = 0; i < n; i++) cin >> a[i];
+#define forntoi(n) for (long long i = n - 1; i >= 0; i--)
+#define sorted(a) sort(a.begin(), a.end())
+#define countn(a,b) count(a.begin(), a.end(), b)
+#define en cout << "\n"
+#define MOD 998244353
+#define ll long long
+const int MAX = 1e5 + 5;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin >> s;
+
+        int res = count(s.begin(), s.end(), '2');
+        int cur = res;
+
+        foriton(s.size()) {
+            if (s[i] == '2') {
+                cur--;
+            }
+            else if (s[i] == '1' || s[i] == '3') {
+                cur++;
+            }
+
+            res = min(res, cur);
+        }
+
+        cout << count(s.begin(), s.end(), '4') + res << "\n";
+    }
+}
